69e2d3f0cbb0a40e43d80cfe8e48edac0056141a
Eric <eric.schmale@gmx.de>
2023-05-19 20:22:39 +0200

    gameloop

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,32 @@ fn main() {
     let player1 = Entity::new(String::from("Nani"));
     let player2 = Entity::new(String::from("COM"));
 
-    println!("---- ROUND {} ----", round);
+    loop {
+        println!("---- ROUND {} ----", round);
 
-    player1.print_stats();
-    player2.print_stats();
+        player1.print_stats();
+        player2.print_stats();
 
-    println!("");
-    println!("Actions: ");
-    println!(" 1 Light Attack");
-    println!(" 2 Heavy Attack");
-    println!(" 3 Spell");
+        println!("");
+        println!("Actions: ");
+        println!(" 1 Light Attack");
+        println!(" 2 Heavy Attack");
+        println!(" 3 Spell");
+        println!(" 4 EXIT");
 
-    let mut line = String::new();
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
-    let mut line = line.trim();
 
-    match line {
-        "1" => {}
-        "2" => {}
-        "3" => {}
-        _ => (),
+        let mut line = String::new();
+        let b1 = std::io::stdin().read_line(&mut line).unwrap();
+        let mut line = line.trim();
+
+        match line {
+            "1" => {}
+            "2" => {}
+            "3" => {}
+            "4" => break,
+            _ => continue,
+        }
+
+        round += 1;
     }
 }
